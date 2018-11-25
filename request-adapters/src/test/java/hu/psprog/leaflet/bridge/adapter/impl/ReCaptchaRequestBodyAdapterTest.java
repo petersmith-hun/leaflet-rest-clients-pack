36e0e6774f4043319586a52ca03833a4e2d81f00
Péter Kovács <peter.kovacs0519@gmail.com>
2018-11-25 17:39:44 +0100

    LFLT-307 Migrate common Leaflet components to OpenJDK 11  - Updated Java version to 11  - Updated some dependencies  - Replaced usages of deprecated classes

--- request-adapters/src/test/java/hu/psprog/leaflet/bridge/adapter/impl/ReCaptchaRequestBodyAdapterTest.java
+++ request-adapters/src/test/java/hu/psprog/leaflet/bridge/adapter/impl/ReCaptchaRequestBodyAdapterTest.java
@@ -5,7 +5,7 @@ import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
