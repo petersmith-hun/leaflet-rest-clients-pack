6425665bb2e203a59313d5392dfa81e6eebe98a7
Péter Kovács <peter.kovacs0519@gmail.com>
2019-02-03 18:07:54 +0100

    LFLT-327 Implement processing contact page  - Added ContactBridgeService and its implementation  - Added new test case

---
+++ backend-rest-client/src/main/java/hu/psprog/leaflet/bridge/service/impl/ContactBridgeServiceImpl.java
@@ -0,0 +1,40 @@
+package hu.psprog.leaflet.bridge.service.impl;
+
+import hu.psprog.leaflet.api.rest.request.contact.ContactRequestModel;
+import hu.psprog.leaflet.bridge.client.BridgeClient;
+import hu.psprog.leaflet.bridge.client.domain.BridgeService;
+import hu.psprog.leaflet.bridge.client.exception.CommunicationFailureException;
+import hu.psprog.leaflet.bridge.client.request.RESTRequest;
+import hu.psprog.leaflet.bridge.client.request.RequestMethod;
+import hu.psprog.leaflet.bridge.config.LeafletPath;
+import hu.psprog.leaflet.bridge.service.ContactBridgeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Implementation of {@link ContactBridgeService}.
+ *
+ * @author Peter Smith
+ */
+@BridgeService(client = "leaflet")
+public class ContactBridgeServiceImpl implements ContactBridgeService {
+
+    private BridgeClient bridgeClient;
+
+    @Autowired
+    public ContactBridgeServiceImpl(BridgeClient bridgeClient) {
+        this.bridgeClient = bridgeClient;
+    }
+
+    @Override
+    public void sendContactRequest(ContactRequestModel contactRequestModel, String recaptchaToken) throws CommunicationFailureException {
+
+        RESTRequest restRequest = RESTRequest.getBuilder()
+                .method(RequestMethod.POST)
+                .path(LeafletPath.CONTACT)
+                .requestBody(contactRequestModel)
+                .recaptchaResponse(recaptchaToken)
+                .build();
+
+        bridgeClient.call(restRequest);
+    }
+}
