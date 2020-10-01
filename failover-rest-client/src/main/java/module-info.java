b931850ff339c38e02532d8fdabfc32472ad076e
Péter Kovács <peter.kovacs0519@gmail.com>
2020-10-01 23:10:59 +0200

    LFLT-431 Update dependencies of Leaflet stack components  - Updated dependency versions  - Bumped component/application version to 1.5.5  - Removed module descriptors

--- failover-rest-client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-open module leaflet.component.rest.failover.client {
-    requires leaflet.component.bridge.api;
-    requires leaflet.component.bridge.implementation;
-    requires spring.beans;
-    requires spring.context;
-    requires leaflet.component.rest.failover.api;
-
-    exports hu.psprog.leaflet.failover.api.client;
-}
--- tlp-rest-api/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module leaflet.component.rest.tlp.api {
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.databind;
-    requires org.apache.commons.lang3;
-    requires spring.context;
-
-    exports hu.psprog.leaflet.tlp.api.domain;
-}
